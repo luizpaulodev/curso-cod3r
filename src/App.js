93a5d3ea605b9ab99f9b2e31aae7a41e9422ac66
unknown <luizpaulodev@gmail.com>
2020-01-24 00:29:13 -0300

    Montando Calculadora

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './App.css'
 
 // import Contador from './components/Contador/Contador';
-import ContadorHook from './components/ContadorHook/ContadorHook';
+// import ContadorHook from './components/ContadorHook/ContadorHook';
+import Calculator from './components/Calculator/Calculator';
 
 function App() {
   return (
     <div className="App">      
       {/* <Contador numeroInicial={0} /> */}
-      <ContadorHook numeroInicial={0} />
+      {/* <ContadorHook numeroInicial={0} /> */}
+
+      <h1>Calculadora</h1>
+      <Calculator />
 
     </div>
   );
